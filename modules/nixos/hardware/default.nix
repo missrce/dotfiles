01f2524b75420c82c81a4550b5428b75ce90a5b7
missrce <vcs@rce.run>
2024-08-31 15:58:30 +0100

    refactor: oops firmware not firmwares

--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     ./bluetooth.nix # bluetooth
-    ./firmwares.nix # firmwares
+    ./firmware.nix # firmware
     ./options.nix # options to set the cpu and gpu
     ./tpm.nix # Trusted Platform Module
     ./yubikey.nix # yubikey device support and management tools
