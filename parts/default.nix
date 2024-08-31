2639f3b01a1582bc239cd52fa7fca42ade068b81
missrce <vcs@rce.run>
2024-08-31 09:05:34 +0100

    refactor: no need for two files here

--- parts/default.nix
+++ parts/default.nix
@@ -1,7 +1,20 @@
-{
-  imports = [
-    ./dev # Development environment for using the shell
+{inputs, ...}: {
+  perSystem = {system, ...}: let
+    config = {
+      allowUnfree = true;
+      allowUnsupportedSystem = true;
+    };
+  in {
+    _module.args = {
+      pkgs = import inputs.nixpkgs {inherit system config;};
+      pkgs-unstable = import inputs.nixpkgs-unstable {inherit system config;};
+      pkgs-master = import inputs.nixpkgs-master {inherit system config;};
+    };
+  };
 
-    ./args.nix # base arguments passed to the flake
+  systems = import inputs.systems;
+
+  imports = [
+    ./dev # Development tooling for the flake
   ];
 }
