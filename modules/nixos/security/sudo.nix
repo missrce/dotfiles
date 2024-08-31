56da0ac34a82c8d2f113629ba82b68a275cb1181
missrce <vcs@rce.run>
2024-08-31 16:18:28 +0100

    refactor: the rules seem to not be taking effect

--- modules/nixos/security/sudo.nix
+++ modules/nixos/security/sudo.nix
@@ -3,16 +3,5 @@
     enable = true;
     execWheelOnly = true;
     wheelNeedsPassword = false;
-    extraRules = [
-      {
-        commands =
-          builtins.map (command: {
-            command = "/run/current-system/sw/bin/${command}";
-            options = ["NOPASSWD"];
-          })
-          ["poweroff" "reboot" "nixos-rebuild" "nix-env" "bandwhich" "mic-light-on" "mic-light-off" "systemctl"];
-        groups = ["wheel"];
-      }
-    ];
   };
 }
