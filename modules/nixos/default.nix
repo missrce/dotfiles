9566ff6520819ef8cd4615d59ca51b5f4e1fcf64
missrce <vcs@rce.run>
2024-08-31 15:35:33 +0100

    feat: import the new config stuff

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,6 +2,8 @@
   imports = [
     ./networking # network configuration
     ./security # NixOS hardening steps
+    ./services # Services for the system and self hosting shenanigans
+    ./hardware # Re-usable hardware configs
 
     ./nix.nix # Modify Nix configuration
     ./catppuccin.nix # Catppuccin setup
