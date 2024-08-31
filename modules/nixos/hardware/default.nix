0f50ffed53767795ae53b39ca83992b60593226f
missrce <vcs@rce.run>
2024-08-31 16:02:21 +0100

    refactor: fix dis

--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -1,8 +1,8 @@
 {
   imports = [
     ./bluetooth.nix # bluetooth
+    ./sound.nix # sound
     ./firmware.nix # firmware
-    ./options.nix # options to set the cpu and gpu
     ./tpm.nix # Trusted Platform Module
     ./yubikey.nix # yubikey device support and management tools
   ];
