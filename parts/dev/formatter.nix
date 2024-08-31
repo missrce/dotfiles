c57498076d8082e74e00ae16ead27794e02767c7
missrce <vcs@rce.run>
2024-08-31 06:25:37 +0100

    feat: style: add formatter config

---
+++ parts/dev/formatter.nix
@@ -0,0 +1,29 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.treefmt-nix.flakeModule ];
+
+  perSystem =
+    { pkgs, config, ... }:
+    {
+      formatter = config.treefmt.build.wrapper;
+
+      treefmt = {
+        projectRootFile = "flake.nix";
+
+        programs = {
+          # Nix formatting
+          nixfmt = {
+            enable = true;
+            package = pkgs.alejandra;
+          };
+          # Enable sanity checks for shell scripts
+          shellcheck.enable = true;
+          # Shell script formatting
+          shfmt = {
+            enable = true;
+            indent_size = 2;
+          };
+        };
+      };
+    };
+}
