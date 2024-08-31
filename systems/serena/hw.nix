d0dd211f1bded9d739ba9cc336f75230d68fce98
missrce <vcs@rce.run>
2024-08-31 15:57:33 +0100

    feat: add more hardware stuff

--- systems/serena/hw.nix
+++ systems/serena/hw.nix
@@ -1,16 +1,67 @@
-{pkgs, ...}: {
-  systemd.services.systemd-udev-settle.enable = false;
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  zramSwap.enable = true;
 
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
-    ];
+  services.fstrim.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware = {
+    nvidia = {
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      powerManagement.enable = true;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = true;
+
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = false;
+        };
+
+        amdgpuBusId = "PCI:16:00:0";
+        nvidiaBusId = "PCI:01:00:0";
+      };
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = false; # me when wayland
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+    };
+
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-extension-layer
+      ];
+    };
   };
+
+  environment.systemPackages = [
+    config.hardware.nvidia.package.lib32.bin
+  ];
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
@@ -30,4 +81,6 @@
       theme = "spinner";
     };
   };
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
