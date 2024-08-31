a36b4b5bbb65f6f840ef05d9d58cb4302a3aeaeb
missrce <vcs@rce.run>
2024-08-31 06:42:05 +0100

    feat: add dev shell

---
+++ parts/dev/shell.nix
@@ -0,0 +1,28 @@
+{
+  perSystem =
+    { pkgs
+    , self'
+    , config
+    , inputs'
+    , ...
+    }:
+    {
+      devShells = {
+        default = pkgs.mkShellNoCC {
+          name = "dotfiles";
+          meta.description = "Development shell for this configuration";
+
+          shellHook = config.pre-commit.installationScript;
+
+          packages = [
+            pkgs.git # Nix flakes require git
+            self'.formatter # nix fmt
+            pkgs.nix-output-monitor # get clean diff between generations
+            inputs'.agenix.packages.agenix # secrets
+          ];
+
+          inputsFrom = [ config.treefmt.build.devShell ];
+        };
+      };
+    };
+}
