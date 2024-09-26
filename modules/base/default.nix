5046d4630371cae467659a65476f7060aa90a4ef
missrce <vcs@rce.run>
2024-09-26 22:12:16 +0100

    refactor: actually import the stuff oh yeah add perless

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -8,5 +8,9 @@
     ./environment
     # Add options
     ./options
+
+    ./perless.nix # no more perl
+    ./programs.nix # basic programs required for ANY install
+    ./secrets.nix # DO NOT SHARE :3c
   ];
 }
