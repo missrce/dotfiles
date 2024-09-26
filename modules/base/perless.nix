01be064cdcf1114c5e651a154a756b1386141045
missrce <vcs@rce.run>
2024-09-26 22:31:11 +0100

    refactor: enable these once it is actually possible to disable Perl and create user accounts

--- modules/base/perless.nix
+++ modules/base/perless.nix
@@ -1,12 +1,14 @@
 # WARNING: it will become very apparent very quickly that this is not a good idea
 # or perhaps a bad idea, or something like that. maybe we should turn back now
 # but im committed to the bit, now we pray i don't endup with a broken system
-{ lib, config, ... }:
-let
+{
+  lib,
+  config,
+  ...
+}: let
   inherit (lib.modules) mkIf mkForce mkMerge;
   inherit (lib.options) mkEnableOption;
-in
-{
+in {
   options.missos.system.banPerl = mkEnableOption "Ban perl from being installed";
 
   config = mkMerge [
@@ -29,7 +31,7 @@ in
       # WARNING: this is an experimental feature, and it is not enabled by default
       # and this will create users with a different script then the perl one
       # so it may cause breakages, be warned
-      systemd.sysusers.enable = true;
+      # systemd.sysusers.enable = true;
 
       system = {
         # WARNING: agenix enables this like such so that user passwords can be encrypted
@@ -42,7 +44,7 @@ in
           # since the original script would only run agenixInstall as a user dep that would
           # give us an error, so we add the agenixInstall as a dep to the "main" agenix
           # activation script such that it will run install for us instead
-          agenix.deps = [ "agenixInstall" ];
+          agenix.deps = ["agenixInstall"];
         };
 
         # Mount /etc as an overlayfs instead of generating it via a perl script.
@@ -51,7 +53,7 @@ in
         etc.overlay.enable = true;
 
         # we can use this to warn us if we have perl installed
-        forbiddenDependenciesRegexes = [ "perl" ];
+        # forbiddenDependenciesRegexes = [ "perl" ];
       };
     })
   ];
