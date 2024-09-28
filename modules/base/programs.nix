24dd2ea9d55536965c5d9b2be15bdbf917ce4a71
missrce <vcs@rce.run>
2024-09-28 22:24:40 +0100

    refactor: more of a home-manager thing

--- modules/base/programs.nix
+++ modules/base/programs.nix
@@ -1,19 +1,22 @@
-{
-  programs = {
-    starship = {
-      enable = true;
-      presets = [
-        "nerd-font-symbols"
-      ];
-    };
+{pkgs, ...}: {
+  # programs = {
+  #   starship = {
+  #     enable = true;
+  #     presets = [
+  #       "nerd-font-symbols"
+  #     ];
+  #   };
 
-    # bash = {
-    #   enable = true;
-    # historyFile = "/dev/null";
-    # historyFileSize = 0;
-    # initExtra = ''
-    #   echo -ne "\e[5 q"
-    # '';
-    # };
-  };
+  # bash = {
+  #   enable = true;
+  # historyFile = "/dev/null";
+  # historyFileSize = 0;
+  # initExtra = ''
+  #   echo -ne "\e[5 q"
+  # '';
+  # };
+  # };
+  environment.systemPackages = with pkgs; [
+    micro
+  ];
 }
