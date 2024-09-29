d869f1c95eee585f1e4d856cb7ca2de3d16dbb49
missrce <vcs@rce.run>
2024-09-29 20:18:28 +0100

    feat: add partitions to luna

--- systems/luna/hw.nix
+++ systems/luna/hw.nix
@@ -31,6 +31,18 @@
     initrd.systemd.enable = true;
   };
 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/B031-FEFC";
+    fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
+  };
+
+  fileSystems."/" = {
+    device = "UUID=2599b621-1dbb-4bf6-8fa0-8dd80b2814e0";
+    fsType = "bcachefs";
+    options = ["noatime" "nodiratime" "nofail"];
+  };
+
   services.power-profiles-daemon.enable = true;
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
