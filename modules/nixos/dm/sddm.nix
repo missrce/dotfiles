19a294c9ab81f2a9875881ce9466a7f74add9b3b
missrce <vcs@rce.run>
2024-09-30 03:51:46 +0100

    refactor: shoudl fix it I hope?

--- modules/nixos/dm/sddm.nix
+++ modules/nixos/dm/sddm.nix
@@ -5,11 +5,15 @@
 }: let
   inherit (config.missos) environment;
 in {
-  services.xserver.enable = true;
+  systemd.services.display-manager.wants = [
+    "systemd-user-sessions.service"
+    "multi-user.target"
+    "network-online.target"
+  ];
   services.displayManager.sddm = {
     enable = environment.loginManager == "sddm";
     package = pkgs.kdePackages.sddm; # allow qt6 themes to work
     wayland.enable = true; # run under wayland rather than X11
-    # settings.General.InputMethod = "";
+    settings.General.InputMethod = "";
   };
 }
