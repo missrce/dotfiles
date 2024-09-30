bc4800fa657164cd4ea033f580f45ac24674d622
missrce <vcs@rce.run>
2024-09-30 02:49:13 +0100

    refactor: whoops

--- systems/luna/default.nix
+++ systems/luna/default.nix
@@ -15,9 +15,10 @@
       hasTPM = true;
     };
     system = {
+      mainUserHashedPassword = "$y$j9T$MARUL6K4RRYl1afeV0IkX1$x8tx0t29j1iNPG.cEdxMZQEWq13n4hdZ5W6EmgfcwGD";
+
       interface.graphical = true;
       yubikeySupport = true;
-      users.mainUserHashedPassword = "$y$j9T$MARUL6K4RRYl1afeV0IkX1$x8tx0t29j1iNPG.cEdxMZQEWq13n4hdZ5W6EmgfcwGD";
       security = {
         auditd = {
           enable = true;
--- systems/serena/default.nix
+++ systems/serena/default.nix
@@ -15,9 +15,10 @@
       hasTPM = true;
     };
     system = {
+      mainUserHashedPassword = "$y$j9T$tIq/hBimd7jvSr2bL/Lm0/$W0tjpjhc2dGy/F8mf4ASBqrGjO9YXUh9cEvAPNA.hvC";
+
       interface.graphical = true;
       yubikeySupport = true;
-      mainUserHashedPassword = "$y$j9T$tIq/hBimd7jvSr2bL/Lm0/$W0tjpjhc2dGy/F8mf4ASBqrGjO9YXUh9cEvAPNA.hvC";
       security = {
         auditd = {
           enable = true;
