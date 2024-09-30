0bb54c4312d3ffc1eabe47edf803c6655d6e7c64
missrce <vcs@rce.run>
2024-09-30 04:10:14 +0100

    refactor: you need to wrap it in brackets

--- modules/nixos/dm/sddm.nix
+++ modules/nixos/dm/sddm.nix
@@ -7,28 +7,26 @@
   inherit (config.missos) environment;
   inherit (lib.modules) mkIf mkForce;
 in {
-  config =
-    mkIf environment.loginManager
-    == "sddm" {
-      services.displayManager.sddm = {
-        enable = true;
-        package = pkgs.kdePackages.sddm; # allow qt6 themes to work
-        wayland.enable = true; # run under wayland rather than X11
-        settings.General.InputMethod = "";
-      };
-      systemd.services.display-manager = {
-        wants = mkForce [
-          "systemd-user-sessions.service"
-        ];
-        after = mkForce [
-          "systemd-user-sessions.service"
-          "getty@tty1.service"
-          "plymouth-quit.service"
-          "systemd-logind.service"
-        ];
-        conflicts = mkForce [
-          "getty@tty1.service"
-        ];
-      };
+  config = mkIf (environment.loginManager == "sddm") {
+    services.displayManager.sddm = {
+      enable = true;
+      package = pkgs.kdePackages.sddm; # allow qt6 themes to work
+      wayland.enable = true; # run under wayland rather than X11
+      settings.General.InputMethod = "";
     };
+    systemd.services.display-manager = {
+      wants = mkForce [
+        "systemd-user-sessions.service"
+      ];
+      after = mkForce [
+        "systemd-user-sessions.service"
+        "getty@tty1.service"
+        "plymouth-quit.service"
+        "systemd-logind.service"
+      ];
+      conflicts = mkForce [
+        "getty@tty1.service"
+      ];
+    };
+  };
 }
