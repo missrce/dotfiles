d85a5dc7ed48ed2e03cdc76da9794a8161303a91
missrce <vcs@rce.run>
2024-09-30 04:38:00 +0100

    refactor: might solve the problem

--- modules/nixos/dm/sddm.nix
+++ modules/nixos/dm/sddm.nix
@@ -5,28 +5,15 @@
   ...
 }: let
   inherit (config.missos) environment;
-  inherit (lib.modules) mkIf mkForce;
+  inherit (lib.modules) mkIf;
 in {
   config = mkIf (environment.loginManager == "sddm") {
+    systemd.services.display-manager.preStart = ''${pkgs.coreutils}/bin/sleep 0.5'';
     services.displayManager.sddm = {
       enable = true;
       package = pkgs.kdePackages.sddm; # allow qt6 themes to work
       wayland.enable = true; # run under wayland rather than X11
       settings.General.InputMethod = "";
-    };
-    systemd.services.display-manager = {
-      wants = mkForce [
-        "systemd-user-sessions.service"
-      ];
-      after = mkForce [
-        "systemd-user-sessions.service"
-        "getty@tty1.service"
-        "plymouth-quit.service"
-        "systemd-logind.service"
-      ];
-      conflicts = mkForce [
-        "getty@tty1.service"
-      ];
     };
   };
 }
