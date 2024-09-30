52d6e909072bb9bcdd1e07ce49526e241d8d18a0
missrce <vcs@rce.run>
2024-09-30 04:09:24 +0100

    refactor: this should fix it

--- modules/nixos/dm/sddm.nix
+++ modules/nixos/dm/sddm.nix
@@ -1,19 +1,34 @@
 {
+  lib,
   pkgs,
   config,
   ...
 }: let
   inherit (config.missos) environment;
+  inherit (lib.modules) mkIf mkForce;
 in {
-  systemd.services.display-manager.wants = [
-    "systemd-user-sessions.service"
-    "multi-user.target"
-    "network-online.target"
-  ];
-  services.displayManager.sddm = {
-    enable = environment.loginManager == "sddm";
-    package = pkgs.kdePackages.sddm; # allow qt6 themes to work
-    wayland.enable = true; # run under wayland rather than X11
-    settings.General.InputMethod = "";
-  };
+  config =
+    mkIf environment.loginManager
+    == "sddm" {
+      services.displayManager.sddm = {
+        enable = true;
+        package = pkgs.kdePackages.sddm; # allow qt6 themes to work
+        wayland.enable = true; # run under wayland rather than X11
+        settings.General.InputMethod = "";
+      };
+      systemd.services.display-manager = {
+        wants = mkForce [
+          "systemd-user-sessions.service"
+        ];
+        after = mkForce [
+          "systemd-user-sessions.service"
+          "getty@tty1.service"
+          "plymouth-quit.service"
+          "systemd-logind.service"
+        ];
+        conflicts = mkForce [
+          "getty@tty1.service"
+        ];
+      };
+    };
 }
