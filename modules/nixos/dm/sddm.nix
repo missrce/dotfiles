ceddc476b79a71d6bfe8b8becdab800092193afa
missrce <vcs@rce.run>
2024-09-30 04:45:15 +0100

    refactor: restart on timeout

--- modules/nixos/dm/sddm.nix
+++ modules/nixos/dm/sddm.nix
@@ -5,10 +5,22 @@
   ...
 }: let
   inherit (config.missos) environment;
-  inherit (lib.modules) mkIf;
+  inherit (lib.modules) mkOverride mkIf;
 in {
   config = mkIf (environment.loginManager == "sddm") {
-    systemd.services.display-manager.preStart = ''${pkgs.coreutils}/bin/sleep 0.5'';
+    systemd.services.display-manager = {
+      serviceConfig = {
+        Restart = mkOverride 50 "on-failure";
+        TimeoutStopSec = 1;
+        SendSIGHUP = true;
+      };
+      after = [
+        "getty@tty1.service"
+      ];
+      conflicts = [
+        "getty@tty1.service"
+      ];
+    };
     services.displayManager.sddm = {
       enable = true;
       package = pkgs.kdePackages.sddm; # allow qt6 themes to work
