b42a32a9b2091cd775b76daaf68fda841de7ebe6
missrce <vcs@rce.run>
2024-10-02 22:29:04 +0100

    feat: customise hyprlock

--- home/missrce/services/wayland/hyprland/hyprlock.nix
+++ home/missrce/services/wayland/hyprland/hyprlock.nix
@@ -2,13 +2,42 @@
   lib,
   osConfig,
   ...
-}:
-let
+}: let
   inherit (lib.modules) mkIf;
   inherit (osConfig.missos) system environment;
 in {
   programs.hyprlock = mkIf (system.interface.graphical && environment.desktop == "Hyprland") {
     enable = true;
+
+    extraConfig = ''
+      background {
+        monitor =
+        color = $base
+      }
+
+      input-field {
+        monitor =
+        placeholder_text = <i>:3</i>
+
+        size = 400, 50
+        outline_thickness = 2
+        dots_size = 0.2
+        dots_center = true
+        fade_on_empty = true
+
+        position = 0, 0
+        halign = center
+        valign = center
+
+        outer_color = $accent
+        inner_color = $surface0
+        font_color = $text
+        check_color = $peach
+        fail_color = $red
+
+        fail_timeout = 500
+      }
+    '';
 
     catppuccin.enable = true;
   };
