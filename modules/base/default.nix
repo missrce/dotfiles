7ca5a1d94dbb7aefce20ab28d6886259bf6c1481
missrce <vcs@rce.run>
2024-10-02 20:27:51 +0100

    feat: activation script for root fixing ssh

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -8,6 +8,7 @@
     ./environment
     # Add options
     ./options
+    ./activation # Activation magic
 
     ./programs.nix # basic programs required for ANY install
     ./secrets.nix # DO NOT SHARE :3c
