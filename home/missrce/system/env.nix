db3ce569cd26e22ab921b7b35c9ee482681ebea6
missrce <vcs@rce.run>
2024-10-02 19:47:10 +0100

    refactor: not needed

--- home/missrce/system/env.nix
+++ home/missrce/system/env.nix
@@ -1,13 +1,11 @@
-{config, ...}: {
-  systemd.user.sessionVariables =
-    config.home.sessionVariables
-    // {
-      EDITOR = "micro";
-      VISUAL = "codium";
-      SUDO_EDITOR = "micro";
+{
+  systemd.user.sessionVariables = {
+    EDITOR = "micro";
+    VISUAL = "codium";
+    SUDO_EDITOR = "micro";
 
-      FLAKE = "/etc/nixos";
-      SYSTEMD_PAGERSECURE = "true";
-      PAGER = "less -FR";
-    };
+    FLAKE = "/etc/nixos";
+    SYSTEMD_PAGERSECURE = "true";
+    PAGER = "less -FR";
+  };
 }
