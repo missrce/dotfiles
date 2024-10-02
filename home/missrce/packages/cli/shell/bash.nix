8205594296a59ac3a92c3ed764c1a97c5a0af5b9
missrce <vcs@rce.run>
2024-10-02 21:02:00 +0100

    refactor: currently the only way to get variables to work to any degree

--- home/missrce/packages/cli/shell/bash.nix
+++ home/missrce/packages/cli/shell/bash.nix
@@ -15,6 +15,9 @@
         eval "$(${lib.getExe config.programs.starship.package} init bash)"
       fi
       echo -ne "\e[5 q"
+      unset __HM_SESS_VARS_SOURCED
+      . "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh"
+      . "${config.home.homeDirectory}/.config/environment.d/10-home-manager.conf"
     '';
   };
 }
