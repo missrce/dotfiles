e39638f4b9476397143fcda218c677acf82787f9
missrce <vcs@rce.run>
2024-10-02 19:40:23 +0100

    refactor: we do some env magic

--- home/missrce/system/env.nix
+++ home/missrce/system/env.nix
@@ -1,11 +1,13 @@
-{
-  home.sessionVariables = {
-    EDITOR = "micro";
-    VISUAL = "codium";
-    SUDO_EDITOR = "micro";
+{config, ...}: {
+  systemd.user.sessionVariables =
+    config.home.sessionVariables
+    // {
+      EDITOR = "micro";
+      VISUAL = "codium";
+      SUDO_EDITOR = "micro";
 
-    FLAKE = "/etc/nixos";
-    SYSTEMD_PAGERSECURE = "true";
-    PAGER = "less -FR";
-  };
+      FLAKE = "/etc/nixos";
+      SYSTEMD_PAGERSECURE = "true";
+      PAGER = "less -FR";
+    };
 }
