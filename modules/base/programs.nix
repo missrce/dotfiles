05e4436f1874beb60f4e9dd21820d9b2c39336c7
missrce <vcs@rce.run>
2024-10-02 20:10:32 +0100

    refactor: no longer needed its in home-manager now

--- modules/base/programs.nix
+++ modules/base/programs.nix
@@ -1,21 +1,4 @@
 {pkgs, ...}: {
-  # programs = {
-  #   starship = {
-  #     enable = true;
-  #     presets = [
-  #       "nerd-font-symbols"
-  #     ];
-  #   };
-
-  # bash = {
-  #   enable = true;
-  # historyFile = "/dev/null";
-  # historyFileSize = 0;
-  # initExtra = ''
-  #   echo -ne "\e[5 q"
-  # '';
-  # };
-  # };
   environment.systemPackages = with pkgs; [
     micro
   ];
