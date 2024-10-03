c6a656aab71dda6043431e71da22306a0bbe5cb8
missrce <vcs@rce.run>
2024-10-03 16:19:55 +0100

    feat: add polkit agent GNOME

--- modules/nixos/security/polkit.nix
+++ modules/nixos/security/polkit.nix
@@ -1,10 +1,28 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }: let
   inherit (lib.modules) mkIf mkDefault;
 in {
+  systemd = mkIf config.missos.system.interface.graphical {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "GNome PolicyKit(ty) :3 Agent";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+    };
+  };
+
   # have polkit log all actions
   security.polkit = {
     enable = true;
