fb8e4985fc525e28f58f79d757e6e0fbc2977b02
missrce <vcs@rce.run>
2024-10-03 16:20:57 +0100

    feat: add seahorse support

--- modules/nixos/security/keyring.nix
+++ modules/nixos/security/keyring.nix
@@ -1,3 +1,10 @@
 {
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+in {
   services.gnome.gnome-keyring.enable = true;
+  programs.seahorse.enable = mkIf config.missos.system.interface.graphical true;
 }
