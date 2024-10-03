2d1b76ae3429ec1fd3844cdc0e02fe9dede9dffe
missrce <vcs@rce.run>
2024-10-03 19:16:34 +0100

    refactor: impermanence magic

--- systems/luna/hw.nix
+++ systems/luna/hw.nix
@@ -46,17 +46,58 @@
     initrd.systemd.enable = true;
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/B031-FEFC";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
+  boot.initrd.luks.devices."luks_root".device = "/dev/disk/by-uuid/37a4bc2a-0892-492c-ae2d-d7cd312406a7";
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-uuid/B031-FEFC";
+      fsType = "vfat";
+      options = ["fmask=0077" "dmask=0077"];
+    };
+
+    "/" = {
+      device = "/dev/mapper/luks_root";
+      fsType = "btrfs";
+      options = ["subvol=root" "noatime" "nodiratime" "nofail"];
+    };
+
+    "/persistent" = {
+      device = "/dev/mapper/luks_root";
+      fsType = "btrfs";
+      options = ["subvol=persistent" "compress=zstd" "noatime" "nodiratime"];
+    };
+
+    "/nix" = {
+      device = "/dev/mapper/luks_root";
+      fsType = "btrfs";
+      options = ["subvol=nix" "compress=zstd" "noatime" "nodiratime"];
+    };
   };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/48608c0f-01df-414b-b500-e6ff2af2f1d0";
-    fsType = "bcachefs";
-    options = ["noatime" "nodiratime" "nofail"];
-  };
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    mkdir -p /btrfs_tmp
+    mount /dev/mapper/luks_root /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
+
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +1); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
 
   services.power-profiles-daemon.enable = true;
 
