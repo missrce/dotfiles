a0c37d07e7f6fda053229360f4569fad4db0f8ed
missrce <vcs@rce.run>
2024-10-03 16:30:43 +0100

    refactor: make sure seahorse gets enabled for ask password

--- modules/nixos/security/keyring.nix
+++ modules/nixos/security/keyring.nix
@@ -6,5 +6,6 @@
   inherit (lib.modules) mkIf;
 in {
   services.gnome.gnome-keyring.enable = true;
+  programs.ssh.enableAskPassword = true;
   programs.seahorse.enable = mkIf config.missos.system.interface.graphical true;
 }
