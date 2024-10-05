df95615b435fb6fb4b11bf9ac681d0578de6997c
missrce <vcs@rce.run>
2024-10-05 04:38:01 +0100

    feat: add isWM scope

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -1,17 +1,35 @@
-{config, lib, ...}: let
-  inherit (lib.options) mkOption;
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.options) mkEnableOption mkOption;
   inherit (lib.types) nullOr enum;
+
+  cfg = config.missos.environment;
 in {
   imports = [
     ./Hyprland
   ];
 
-  options.missos.environment.desktop = mkOption {
-    type = nullOr (enum [
-      "none"
-      "Hyprland"
-    ]);
-    default = if config.missos.system.interface.graphical then "Hyprland" else "none";
-    description = "The desktop to be used by the system.";
+  options.missos.environment = {
+    desktop = mkOption {
+      type = nullOr (enum [
+        "none"
+        "Hyprland"
+        "sway"
+      ]);
+      default =
+        if config.missos.system.interface.graphical
+        then "Hyprland"
+        else "none";
+      description = "The desktop to be used by the system.";
+    };
+
+    isWM =
+      mkEnableOption "Inferred based on the desktop environment."
+      // {
+        default = cfg.desktop == "Hyprland" || cfg.desktop == "sway";
+      };
   };
 }
