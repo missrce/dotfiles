aff2d164a7c0279e2dad43fd6c2c4b55f253f32c
missrce <vcs@rce.run>
2024-10-05 13:15:52 +0100

    fix: its atuin.nix not atuin

--- home/missrce/packages/cli/default.nix
+++ home/missrce/packages/cli/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     ./shell # different shells
-    ./atuin # shell history
+    ./atuin.nix # shell history
     ./bat.nix # cat with syntax highlighting
     ./fd.nix # user-friendly find
     ./freeze.nix # turn yo terminal and code into SVGs
