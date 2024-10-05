b0939d947052ee5080d98fb69e25b5da800ec398
missrce <vcs@rce.run>
2024-10-05 05:04:24 +0100

    refactor: namespace to WMs only incase I use a DE

--- home/missrce/services/wayland/clipboard.nix
+++ home/missrce/services/wayland/clipboard.nix
@@ -3,14 +3,14 @@
   pkgs,
   osConfig,
   ...
-}:
-let
+}: let
   inherit (lib.meta) getExe getExe';
   inherit (lib.modules) mkIf;
   inherit (lib.services) mkGraphicalService;
-in
-{
-  config = mkIf osConfig.missos.system.interface.graphical {
+
+  inherit (osConfig.missos) system environment;
+in {
+  config = mkIf (system.interface.graphical && environment.isWM) {
     systemd.user.services.cliphist = mkGraphicalService {
       Unit.Description = "Clipboard history service";
       Service = {
