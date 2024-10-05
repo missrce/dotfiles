40dc0761cbfd8ef479dd15b02f6d58b5196d4441
missrce <vcs@rce.run>
2024-10-05 13:15:17 +0100

    fix: import atuin correctly

--- home/missrce/packages/cli/default.nix
+++ home/missrce/packages/cli/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./shell # different shells
+    ./atuin # shell history
     ./bat.nix # cat with syntax highlighting
     ./fd.nix # user-friendly find
     ./freeze.nix # turn yo terminal and code into SVGs
--- home/missrce/packages/cli/shell/default.nix
+++ home/missrce/packages/cli/shell/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./starship # Blazingly fast RS prompt 🦀
-    ./atuin # Shell history
 
     ./bash.nix
   ];
