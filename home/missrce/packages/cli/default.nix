b2655da9f011dd1c235a37a27a7ffd12f7821fff
missrce <vcs@rce.run>
2024-10-06 19:18:32 +0100

    refactor: better system of managing home cli packages

--- home/missrce/packages/cli/default.nix
+++ home/missrce/packages/cli/default.nix
@@ -1,8 +1,15 @@
 {
+  osConfig,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkMerge mkIf;
+
+  inherit (osConfig.missos) system environment;
+in {
   imports = [
     ./shell # different shells
-
-    ./wm.nix # Window Manager specific packages
 
     ./atuin.nix # shell history
     ./hyfetch.nix # I use NixOS btw,
@@ -11,4 +18,17 @@
     ./freeze.nix # turn yo terminal and code into SVGs
     ./fzf.nix # command-line fuzzy finder
   ];
+
+  home.packages = mkIf system.interface.graphical (
+    mkMerge [
+      # Packages for all graphical environments
+      [pkgs.libnotify]
+
+      # Additional packages for WM environments
+      (lib.mkIf environment.isWM [
+        pkgs.wl-clipboard
+        pkgs.cliphist
+      ])
+    ]
+  );
 }
