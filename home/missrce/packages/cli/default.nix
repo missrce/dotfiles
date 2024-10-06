07fd544324f48408a569b70eaa59fd51fb5a4df2
missrce <vcs@rce.run>
2024-10-06 18:59:44 +0100

    feat: I use NixOS btw,

--- home/missrce/packages/cli/default.nix
+++ home/missrce/packages/cli/default.nix
@@ -5,6 +5,7 @@
     ./wm.nix # Window Manager specific packages
 
     ./atuin.nix # shell history
+    ./hyfetch.nix # I use NixOS btw,
     ./bat.nix # cat with syntax highlighting
     ./fd.nix # user-friendly find
     ./freeze.nix # turn yo terminal and code into SVGs
