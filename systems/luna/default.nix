0b4dc4987613fe5d13344f4a4be4c58fe27b3ade
missrce <vcs@rce.run>
2024-10-08 22:42:29 +0100

    refactor: fix: it's browser.chromium not browser.chromium.enable

--- systems/luna/default.nix
+++ systems/luna/default.nix
@@ -15,7 +15,7 @@
       hasTPM = true;
     };
     programs = {
-      browser.chromium.enable = true;
+      browser.chromium = true;
     };
     system = {
       mainUserHashedPassword = "$y$j9T$MARUL6K4RRYl1afeV0IkX1$x8tx0t29j1iNPG.cEdxMZQEWq13n4hdZ5W6EmgfcwGD";
