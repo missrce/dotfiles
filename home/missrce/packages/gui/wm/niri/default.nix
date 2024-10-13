2dd2b9f21697d0b0ced1d374397cbab222bb85b0
missrce <vcs@rce.run>
2024-10-13 17:15:45 +0100

    refactor: should work actually

--- home/missrce/packages/gui/wm/niri/default.nix
+++ home/missrce/packages/gui/wm/niri/default.nix
@@ -11,7 +11,7 @@ in {
   programs.niri = mkIf (desktop == "niri") {
     settings = {
       prefer-no-csd = true;
-      screenshot-path = "${config.xdg.userDirs.extraConfig.XDG_SCREENSHOTS_DIR}/screenshot-%d\/%m\/%Y-%H:%M:%S.png";
+      screenshot-path = ''${config.xdg.userDirs.extraConfig.XDG_SCREENSHOTS_DIR}/screenshot-%d\/%m\/%Y-%H:%M:%S.png'';
 
       input = {focus-follows-mouse.enable = true;};
       binds = import ./binds.nix {inherit osConfig config pkgs lib;};
