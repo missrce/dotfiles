047bc4a2d677413bd9ec900c6255a658e643ed1b
missrce <vcs@rce.run>
2024-10-13 20:31:52 +0100

    refactor: too big font size

--- home/missrce/packages/gui/launchers/fuzzel.nix
+++ home/missrce/packages/gui/launchers/fuzzel.nix
@@ -1,3 +1,14 @@
-{osConfig, ...}: {
-  programs.fuzzel.enable = osConfig.missos.programs.launcher == "fuzzel";
+{
+  osConfig,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+in {
+  programs.fuzzel = mkIf (osConfig.missos.programs.launcher == "fuzzel") {
+    enable = true;
+    settings = {
+      main.font = "${builtins.elemAt osConfig.fonts.fontconfig.defaultFonts.sansSerif 0}:size=11";
+    };
+  };
 }
