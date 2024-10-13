4ee36df80477993802d62723b9825d3884aeaa20
missrce <vcs@rce.run>
2024-10-13 18:08:34 +0100

    feat: import launchers

--- home/missrce/packages/gui/default.nix
+++ home/missrce/packages/gui/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./browser # Get those web browsers, we must be chronically online!
     ./terminals # Terminal emulators
+    ./launchers # Launchers for applications
     ./wm # Configure the desktop experience
   ];
 }
