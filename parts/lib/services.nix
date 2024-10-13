5e22cb61ecd44573d0886cae5e01bd0a759ac2ee
missrce <vcs@rce.run>
2024-10-13 03:22:06 +0100

    refactor: remove hyprland func

--- parts/lib/services.nix
+++ parts/lib/services.nix
@@ -1,17 +1,11 @@
-{lib}: let 
+{lib}: let
   inherit (lib.attrsets) recursiveUpdate;
 
   mkGraphicalService = recursiveUpdate {
-    Unit.PartOf = [ "graphical-session.target" ];
-    Unit.After = [ "graphical-session.target" ];
-    Install.WantedBy = [ "graphical-session.target" ];
-  };
-
-  mkHyprlandService = recursiveUpdate {
-    Unit.PartOf = [ "graphical-session.target" ];
-    Unit.After = [ "graphical-session.target" ];
-    Install.WantedBy = [ "hyprland-session.target" ];
+    Unit.PartOf = ["graphical-session.target"];
+    Unit.After = ["graphical-session.target"];
+    Install.WantedBy = ["graphical-session.target"];
   };
 in {
-  inherit mkGraphicalService mkHyprlandService;
+  inherit mkGraphicalService;
 }
