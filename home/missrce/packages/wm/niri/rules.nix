9d3ebcfec1daa9931dc7b88259583da28094b78d
missrce <vcs@rce.run>
2024-10-13 20:09:37 +0100

    refactor: bruh

--- home/missrce/packages/wm/niri/rules.nix
+++ home/missrce/packages/wm/niri/rules.nix
@@ -1,6 +1,11 @@
 [
   {
-    geometry-corner-radius = 4;
+    geometry-corner-radius = {
+      top-left = 4;
+      bottom-left = 4;
+      top-right = 4;
+      bottom-right = 4;
+    };
     clip-to-geometry = true;
   }
 ]
