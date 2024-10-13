352f9daf973522cfee8c6e1970a412d5aa4242d7
missrce <vcs@rce.run>
2024-10-13 03:56:51 +0100

    refactor: remove hyprland cachix

--- modules/base/nix/substituters.nix
+++ modules/base/nix/substituters.nix
@@ -5,7 +5,6 @@
       "https://nix-community.cachix.org" # nix-community cache
       "https://nixpkgs-unfree.cachix.org" # unfree-package cache
       "https://pre-commit-hooks.cachix.org" # pre commit hooks
-      "https://hyprland.cachix.org" # hyprland
     ];
 
     trusted-public-keys = [
@@ -13,7 +12,6 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 }
