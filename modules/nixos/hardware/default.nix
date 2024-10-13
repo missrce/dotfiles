9bbece38453d5c330bbebf8971f2fc6f89bd4db2
missrce <vcs@rce.run>
2024-10-13 03:04:15 +0100

    refactor: no more udev-settle

--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -7,4 +7,6 @@
     ./yubikey.nix # yubikey device support and management tools
     ./filesystems.nix # Filesystem support
   ];
+
+  systemd.services.systemd-udev-settle.enable = false; # We do not need to wait for all hardware to initialise completely
 }
