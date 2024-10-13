dc3b3f80a8170b88de0d182e30b38223ca7de005
missrce <vcs@rce.run>
2024-10-13 03:08:37 +0100

    refactor: new flake

--- flake.nix
+++ flake.nix
@@ -4,70 +4,29 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} {imports = [./parts];};
 
   inputs = {
-    # Package suppliers
+    # package suppliers
+    nixpkgs-master.url = "github:NixOS/nixpkgs";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nixpkgs-master = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-    };
+    # for other inputs to follow preventing multiple copies of systems when one is only needed.
+    systems.url = "github:nix-systems/default-linux";
 
-    nixpkgs-unstable = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      ref = "nixpkgs-unstable";
-    };
-
-    nixpkgs = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      ref = "nixos-unstable";
-    };
-
-    # For other inputs to follow preventing multiple copies of systems when one is only needed.
-
-    systems = {
-      type = "github";
-      owner = "nix-systems";
-      repo = "default-linux";
-    };
-
-    # For other inputs to follow preventing multiple copies of flake-utils when one is only needed.
-
-    flake-utils = {
-      type = "github";
-      owner = "numtide";
-      repo = "flake-utils";
-      inputs.systems.follows = "systems";
-    };
-
-    # Minimal flake wrapper
-
+    # minimal flake wrapper
     flake-parts = {
-      type = "github";
-      owner = "hercules-ci";
-      repo = "flake-parts";
+      url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
-    # Tree-wide formatter
-
+    # tree-wide formatter
     treefmt-nix = {
-      type = "github";
-      owner = "numtide";
-      repo = "treefmt-nix";
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Git commit hooks management
-
+    # git commit hooks management
     git-hooks = {
-      type = "github";
-      owner = "cachix";
-      repo = "git-hooks.nix";
-
+      url = "github:cachix/git-hooks.nix";
       inputs = {
         nixpkgs.follows = "nixpkgs-unstable";
         nixpkgs-stable.follows = "";
@@ -76,12 +35,8 @@
     };
 
     # age-encrypted secrets for NixOS
-
     agenix = {
-      type = "github";
-      owner = "ryantm";
-      repo = "agenix";
-
+      url = "github:ryantm/agenix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         darwin.follows = "";
@@ -90,47 +45,32 @@
       };
     };
 
-    # Userspace management
-
+    # userspace management
     home-manager = {
-      type = "github";
-      owner = "nix-community";
-      repo = "home-manager";
+      url = "home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # free wallpaper storage :100::money_mouth::fire:
-
     wallpkgs = {
-      type = "github";
-      owner = "NotAShelf"; # urm ackshually you are
-      repo = "wallpkgs";
-
+      url = "github:NotAShelf/wallpkgs";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         systems.follows = "systems";
       };
     };
 
-    # manage disk partition layouts declaratively
-
+    # manage disk partition layouts
     disko = {
-      type = "github";
-      owner = "nix-community";
-      repo = "disko";
-
+      url = "github:nix-community/disko";
       inputs = {
         nixpkgs.follows = "nixpkgs-unstable";
       };
     };
 
-    # Secure Boot for NixOS
-
+    # secure boot for nixos
     lanzaboote = {
-      type = "github";
-      owner = "nix-community";
-      repo = "lanzaboote";
-
+      url = "github:nix-community/lanzaboote";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
@@ -139,17 +79,7 @@
       };
     };
 
-    # Hyprland flake
-
-    hyprland = {
-      type = "git";
-      url = "https://github.com/hyprwm/Hyprland";
-      submodules = true;
-
-      inputs.systems.follows = "systems";
-    };
-
-    # Catppuccin Theming
+    # catppuccin theming
     catppuccin = {
       type = "github";
       owner = "catppuccin";
