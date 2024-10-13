ad73e9de3fd71dfba18f26b73c6f2b75376dcb08
missrce <vcs@rce.run>
2024-10-13 18:22:38 +0100

    feat: style: better foot theme

--- home/missrce/packages/gui/terminals/foot.nix
+++ home/missrce/packages/gui/terminals/foot.nix
@@ -1,6 +1,15 @@
 {
   osConfig,
+  lib,
   ...
-}: {
-  programs.foot.enable = osConfig.missos.programs.terminal == "foot";
+}: let
+  inherit (lib.modules) mkIf;
+in {
+  programs.foot = mkIf (osConfig.missos.programs.terminal == "foot") {
+    enable = true;
+    settings = {
+      main.font = "${builtins.elemAt osConfig.fonts.fontconfig.defaultFonts.monospace 0}:size=11";
+      cursor.style = "beam";
+    };
+  };
 }
