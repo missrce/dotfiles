d51e37adec5975be94f18dad1493d398dee6d006
missrce <vcs@rce.run>
2024-10-13 15:54:14 +0100

    refactor: fix the bindings for playerctl and getexe

--- home/missrce/packages/gui/wm/niri/binds.nix
+++ home/missrce/packages/gui/wm/niri/binds.nix
@@ -3,67 +3,69 @@
   pkgs,
   lib,
   ...
-}:
-with config.lib.niri.actions; {
-  # Workspaces
-  "Mod+1".action = focus-workspace 1;
-  "Mod+2".action = focus-workspace 2;
-  "Mod+3".action = focus-workspace 3;
-  "Mod+4".action = focus-workspace 4;
-  "Mod+5".action = focus-workspace 5;
-  "Mod+6".action = focus-workspace 6;
-  "Mod+7".action = focus-workspace 7;
-  "Mod+8".action = focus-workspace 8;
-  "Mod+9".action = focus-workspace 9;
-  "Mod+0".action = focus-workspace 10;
+}: let
+  inherit (lib.meta) getExe getExe';
+in
+  with config.lib.niri.actions; {
+    # Workspaces
+    "Mod+1".action = focus-workspace 1;
+    "Mod+2".action = focus-workspace 2;
+    "Mod+3".action = focus-workspace 3;
+    "Mod+4".action = focus-workspace 4;
+    "Mod+5".action = focus-workspace 5;
+    "Mod+6".action = focus-workspace 6;
+    "Mod+7".action = focus-workspace 7;
+    "Mod+8".action = focus-workspace 8;
+    "Mod+9".action = focus-workspace 9;
+    "Mod+0".action = focus-workspace 10;
 
-  # Window state controls
-  "Mod+W".action = close-window;
-  "Mod+F".action = fullscreen-window;
-  "Mod+Comma".action = consume-window-into-column;
-  "Mod+Period".action = expel-window-from-column;
+    # Window state controls
+    "Mod+W".action = close-window;
+    "Mod+F".action = fullscreen-window;
+    "Mod+Comma".action = consume-window-into-column;
+    "Mod+Period".action = expel-window-from-column;
 
-  # Applications
-  "Alt+T".action = spawn "${lib.getExe pkgs.foot}";
+    # Applications
+    "Alt+T".action = spawn "${lib.getExe pkgs.foot}";
 
-  # Volume keys
+    # Volume keys
 
-  "XF86AudioRaiseVolume" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.wireplumber "wpctl"}" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+";
-  };
-  "XF86AudioLowerVolume" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.wireplumber "wpctl"}" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-";
-  };
-  "XF86AudioMute" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.wireplumber "wpctl"}" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle";
-  };
-  "XF86AudioMicMute" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.wireplumber "wpctl"}" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle";
-  };
+    "XF86AudioRaiseVolume" = {
+      allow-when-locked = true;
+      action = spawn "${getExe' pkgs.wireplumber "wpctl"}" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+";
+    };
+    "XF86AudioLowerVolume" = {
+      allow-when-locked = true;
+      action = spawn "${getExe' pkgs.wireplumber "wpctl"}" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-";
+    };
+    "XF86AudioMute" = {
+      allow-when-locked = true;
+      action = spawn "${getExe' pkgs.wireplumber "wpctl"}" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle";
+    };
+    "XF86AudioMicMute" = {
+      allow-when-locked = true;
+      action = spawn "${getExe' pkgs.wireplumber "wpctl"}" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle";
+    };
 
-  # Player keys
-  "XF86AudioPlay" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.playerctl}" "play-pause";
-  };
-  "XF86AudioPause" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.playerctl}" "play-pause";
-  };
-  "XF86AudioStop" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.playerctl}" "stop";
-  };
-  "XF86AudioNext" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.playerctl}" "next";
-  };
-  "XF86AudioPrev" = {
-    allow-when-locked = true;
-    action = spawn "${lib.getExe' pkgs.playerctl}" "previous";
-  };
-}
+    # Player keys
+    "XF86AudioPlay" = {
+      allow-when-locked = true;
+      action = spawn "${getExe pkgs.playerctl}" "play-pause";
+    };
+    "XF86AudioPause" = {
+      allow-when-locked = true;
+      action = spawn "${getExe pkgs.playerctl}" "play-pause";
+    };
+    "XF86AudioStop" = {
+      allow-when-locked = true;
+      action = spawn "${getExe pkgs.playerctl}" "stop";
+    };
+    "XF86AudioNext" = {
+      allow-when-locked = true;
+      action = spawn "${getExe pkgs.playerctl}" "next";
+    };
+    "XF86AudioPrev" = {
+      allow-when-locked = true;
+      action = spawn "${getExe pkgs.playerctl}" "previous";
+    };
+  }
