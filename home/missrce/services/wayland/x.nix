f647f27bb4a07a7eb88ad99dc383a3e035222837
missrce <vcs@rce.run>
2024-10-15 02:39:16 +0100

    feat: add the DISPLAY to environment variables

--- home/missrce/services/wayland/x.nix
+++ home/missrce/services/wayland/x.nix
@@ -1,6 +1,13 @@
-{pkgs, lib, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   inherit (lib.services) mkGraphicalService;
 in {
+  home.sessionVariables = {
+    DISPLAY = ":0"; # The default output for xwayland-satellite
+  };
   systemd.user.services.xwayland-satellite = mkGraphicalService {
     Unit.Description = "Rootless Xwayland integration to any Wayland compositor implementing xdg_wm_base";
     Service = {
