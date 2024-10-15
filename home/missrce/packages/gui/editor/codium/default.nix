02ec29cac4382a2dfa9f4c9a4ab61a9a34acd3f1
missrce <vcs@rce.run>
2024-10-15 01:43:42 +0100

    refactor: needs osConfig

--- home/missrce/packages/gui/editor/codium/default.nix
+++ home/missrce/packages/gui/editor/codium/default.nix
@@ -7,7 +7,7 @@
     enableExtensionUpdateCheck = false;
     # https://github.com/nix-community/home-manager/issues/4394#issuecomment-1712909231
     mutableExtensionsDir = false;
-    userSettings = import ./settings.nix { inherit lib pkgs; };
+    userSettings = import ./settings.nix { inherit osConfig pkgs lib; };
     extensions = pkgs.callPackage ./extensions.nix {
       extensions = (inputs.vscode-extensions.extensions.${osConfig.nixpkgs.system}.forVSCodeVersion pkgs.vscodium.version).vscode-marketplace;
     };
