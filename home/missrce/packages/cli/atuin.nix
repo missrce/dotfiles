ae356bae40543cb4f8b01f2e36eb9de095dccd06
missrce <vcs@rce.run>
2024-10-15 21:10:58 +0100

    feat: use the daemon

--- home/missrce/packages/cli/atuin.nix
+++ home/missrce/packages/cli/atuin.nix
@@ -1,4 +1,6 @@
-{
+{config, lib, ...}: let
+  inherit (lib.meta) getExe;
+in {
   programs.atuin = {
     enable = true;
 
@@ -13,6 +15,43 @@
       common_prefix = [
         "sudo"
       ];
+      daemon.systemd_socket = true;
+    };
+  };
+  home.sessionVariables = {
+    ATUIN_DAEMON__SOCKET_PATH = "$XDG_RUNTIME_DIR/atuin.sock";
+  };
+  systemd.user.services.atuin-daemon = {
+    Unit = {
+      Description = "atuin daemon";
+      Requires = ["atuin-daemon.socket"];
+    };
+    Install = {
+      Also = ["atuin-daemon.socket"];
+      WantedBy = ["default.target"];
+    };
+    Service = {
+      ExecStart = "${getExe config.programs.atuin.package} daemon";
+      Environment = [
+        "ATUIN_LOG=info"
+        "ATUIN_DAEMON__SOCKET_PATH=%t/atuin.sock"
+      ];
+      Restart = "on-failure";
+      RestartSteps = 3;
+      RestartMaxDelaySec = 6;
+    };
+  };
+  systemd.user.sockets.atuin-daemon = {
+    Unit = {
+      Description = "atuin daemon socket";
+    };
+    Install = {
+      WantedBy = ["sockets.target"];
+    };
+    Socket = {
+      ListenStream = "%t/atuin.sock";
+      SocketMode = "0600";
+      RemoveOnStop = true;
     };
   };
 }
