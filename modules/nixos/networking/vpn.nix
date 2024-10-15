d3e13c3ba2a0c566511b589db833f1ff1e85c58c
missrce <vcs@rce.run>
2024-10-15 22:26:00 +0100

    refactor: fix the settings

--- modules/nixos/networking/vpn.nix
+++ modules/nixos/networking/vpn.nix
@@ -4,4 +4,7 @@
     enable = true;
     package = if config.missos.system.interface.graphical then pkgs.mullvad-vpn else pkgs.mullvad;
   };
+  networking.wireguard.enable = true;
+  networking.iproute2.enable = true;
+  networking.firewall.checkReversePath = "loose";
 }
