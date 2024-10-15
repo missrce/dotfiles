19b2446838ca720d4401ff6ac32bffe59d85144e
missrce <vcs@rce.run>
2024-10-15 21:43:00 +0100

    refactor: disable by default it prevents wayland supporting apps using wayland

--- home/missrce/services/wayland/x.nix
+++ home/missrce/services/wayland/x.nix
@@ -7,9 +7,9 @@
 
   inherit (lib.meta) getExe;
 in {
-  home.sessionVariables = {
-    DISPLAY = ":0"; # The default output for xwayland-satellite
-  };
+  # home.sessionVariables = {
+  #   DISPLAY = ":0"; # The default output for xwayland-satellite
+  # };
   systemd.user.services.xwayland-satellite = mkGraphicalService {
     Unit.Description = "Rootless Xwayland integration to any Wayland compositor implementing xdg_wm_base";
     Service = {
