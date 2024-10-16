517b69eeabfec78c939e06b451498f7aad9d5ee5
missrce <vcs@rce.run>
2024-10-16 01:31:49 +0100

    refactor: in the wise words of Linus Torvalds
    if you're not going to play nicely with the mainline workflow maybe be apart of another workflow

--- home/missrce/packages/cli/atuin.nix
+++ home/missrce/packages/cli/atuin.nix
@@ -1,8 +1,9 @@
-{config, lib, ...}: let
-  inherit (lib.meta) getExe;
-in {
+# {config, lib, ...}: let
+#   inherit (lib.meta) getExe;
+# in
+{
   programs.atuin = {
-    enable = true;
+    enable = false;
 
     flags = ["--disable-ctrl-r"];
 
@@ -18,40 +19,40 @@ in {
       daemon.systemd_socket = true;
     };
   };
-  home.sessionVariables = {
-    ATUIN_DAEMON__SOCKET_PATH = "$XDG_RUNTIME_DIR/atuin.sock";
-  };
-  systemd.user.services.atuin-daemon = {
-    Unit = {
-      Description = "atuin daemon";
-      Requires = ["atuin-daemon.socket"];
-    };
-    Install = {
-      Also = ["atuin-daemon.socket"];
-      WantedBy = ["default.target"];
-    };
-    Service = {
-      ExecStart = "${getExe config.programs.atuin.package} daemon";
-      Environment = [
-        "ATUIN_LOG=info"
-        "ATUIN_DAEMON__SOCKET_PATH=%t/atuin.sock"
-      ];
-      Restart = "on-failure";
-      RestartSteps = 3;
-      RestartMaxDelaySec = 6;
-    };
-  };
-  systemd.user.sockets.atuin-daemon = {
-    Unit = {
-      Description = "atuin daemon socket";
-    };
-    Install = {
-      WantedBy = ["sockets.target"];
-    };
-    Socket = {
-      ListenStream = "%t/atuin.sock";
-      SocketMode = "0600";
-      RemoveOnStop = true;
-    };
-  };
+  # home.sessionVariables = {
+  #   ATUIN_DAEMON__SOCKET_PATH = "$XDG_RUNTIME_DIR/atuin.sock";
+  # };
+  # systemd.user.services.atuin-daemon = {
+  #   Unit = {
+  #     Description = "atuin daemon";
+  #     Requires = ["atuin-daemon.socket"];
+  #   };
+  #   Install = {
+  #     Also = ["atuin-daemon.socket"];
+  #     WantedBy = ["default.target"];
+  #   };
+  #   Service = {
+  #     ExecStart = "${getExe config.programs.atuin.package} daemon";
+  #     Environment = [
+  #       "ATUIN_LOG=info"
+  #       "ATUIN_DAEMON__SOCKET_PATH=%t/atuin.sock"
+  #     ];
+  #     Restart = "on-failure";
+  #     RestartSteps = 3;
+  #     RestartMaxDelaySec = 6;
+  #   };
+  # };
+  # systemd.user.sockets.atuin-daemon = {
+  #   Unit = {
+  #     Description = "atuin daemon socket";
+  #   };
+  #   Install = {
+  #     WantedBy = ["sockets.target"];
+  #   };
+  #   Socket = {
+  #     ListenStream = "%t/atuin.sock";
+  #     SocketMode = "0600";
+  #     RemoveOnStop = true;
+  #   };
+  # };
 }
