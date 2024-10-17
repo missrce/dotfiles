bcb83b791524e7bfdf53a7250a1a23cdf425840b
missrce <vcs@rce.run>
2024-10-17 17:03:12 +0100

    feat: add primary rendering device

--- modules/base/options/device.nix
+++ modules/base/options/device.nix
@@ -1,14 +1,19 @@
 {lib, ...}: let
-  inherit (lib.types) enum;
+  inherit (lib.types) enum string nullOr;
   inherit (lib.options) mkOption;
 in {
   options.missos = {
-    device.type = mkOption {
-      type = enum [
-        "laptop"
-        "desktop"
-        "server"
-      ];
+    device = {
+      primaryRenderDevice = mkOption {
+        type = nullOr string;
+      };
+      type = mkOption {
+        type = enum [
+          "laptop"
+          "desktop"
+          "server"
+        ];
+      };
     };
   };
 }
