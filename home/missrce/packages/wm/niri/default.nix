d4aa98075b8aae3d483dbd41bb404478146c16c7
missrce <vcs@rce.run>
2024-10-17 17:29:27 +0100

    feat: use the primaryRenderDevice

--- home/missrce/packages/wm/niri/default.nix
+++ home/missrce/packages/wm/niri/default.nix
@@ -6,14 +6,15 @@
   ...
 }: let
   inherit (lib.modules) mkIf;
-  inherit (osConfig.missos.environment) desktop;
+  inherit (osConfig.missos) environment device;
 in {
-  programs.niri = mkIf (desktop == "niri") {
+  programs.niri = mkIf (environment.desktop == "niri") {
     settings = {
       hotkey-overlay.skip-at-startup = true;
       prefer-no-csd = true;
       screenshot-path = "${config.xdg.userDirs.extraConfig.XDG_SCREENSHOTS_DIR}/screenshot-%d-%m-%Y-%H:%M:%S.png";
       animations.slowdown = 0.7;
+      debug.render-drm-device = device.primaryRenderDevice;
 
       input = import ./input.nix;
       layout = import ./layout.nix {inherit osConfig lib;};
