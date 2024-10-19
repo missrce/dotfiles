05d03ce17002101dc05f718887b408a5376a5c9c
missrce <vcs@rce.run>
2024-10-19 21:32:27 +0100

    feat: add the correct flake env var

--- home/missrce/system/env.nix
+++ home/missrce/system/env.nix
@@ -1,10 +1,10 @@
-{
+{config, ...}: {
   home.sessionVariables = {
     EDITOR = "micro";
     VISUAL = "codium";
     SUDO_EDITOR = "micro";
 
-    FLAKE = "/etc/nixos";
+    FLAKE = "${config.xdg.userDirs.extraConfig.XDG_DEV_DIR}/dotfiles";
     SYSTEMD_PAGERSECURE = "true";
     PAGER = "less -FR";
   };
