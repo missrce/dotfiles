501f49fbc461a044e78be6214bde328898c2f7f2
missrce <vcs@rce.run>
2024-10-20 03:41:50 +0100

    style: remove newline

--- systems/lotus/default.nix
+++ systems/lotus/default.nix
@@ -12,7 +12,6 @@
     };
     system = {
       mainUserHashedPassword = "$y$j9T$g1yCzDtif3jVZ2fwqupAS1$2tYtS/GQT.BqUjFU555I.gGHCj9eNd.gpChMWh/zox1";
-
       security = {
         auditd = {
           enable = true;
