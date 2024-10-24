fcfc5f70a3d9f4c0faaca838d93b56d1efb50c0b
missrce <vcs@rce.run>
2024-10-25 00:58:57 +0100

    refactor: should be using buildInputs

--- parts/templates/rust/shell.nix
+++ parts/templates/rust/shell.nix
@@ -3,14 +3,16 @@
   rustfmt,
   callPackage,
   rust-analyzer,
-}:
-let
-  mainPkg = callPackage ./default.nix { };
+}: let
+  mainPkg = callPackage ./default.nix {};
 in
-mainPkg.overrideAttrs (oa: {
-  nativeBuildInputs = [
-    clippy
-    rustfmt
-    rust-analyzer
-  ] ++ (oa.nativeBuildInputs or [ ]);
-})
+  mainPkg.overrideAttrs (oa: {
+    buildInputs =
+      [
+        clippy
+        rustfmt
+        rust-analyzer
+      ]
+      ++ (oa.buildInputs or [])
+      ++ (oa.nativeBuildInputs or []);
+  })
