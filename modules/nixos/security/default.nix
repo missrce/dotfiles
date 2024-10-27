3b5b75f3d281ffeb7e46577da3911146c0146885
missrce <vcs@rce.run>
2024-10-27 08:43:19 +0000

    refactor: disable hardened its not used anymore

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -1,8 +1,8 @@
 # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/hardened.nix
 # https://github.com/fort-nix/nix-bitcoin/blob/master/modules/presets/hardened-extended.nix
-{modulesPath, ...}: {
+{...}: {
   imports = [
-    "${modulesPath}/profiles/hardened.nix"
+    # "${modulesPath}/profiles/hardened.nix"
     ./memalloc.nix
     ./kernel.nix
     ./secureboot.nix
