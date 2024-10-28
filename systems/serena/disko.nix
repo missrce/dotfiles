538660f5cb8e58ba276e491cf71a347806deb905
missrce <vcs@rce.run>
2024-10-28 07:52:01 +0000

    refactor: you should work

--- systems/serena/disko.nix
+++ systems/serena/disko.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_2000GB_22260N801935";
+        device = "/dev/disk/by-id/nvme-Seagate_FireCuda_SE_SSD_ZP1000GM30033_7VQ02747";
         type = "disk";
         content = {
           type = "gpt";
@@ -17,12 +17,33 @@
                 mountOptions = ["fmask=0077" "umask=0077"];
               };
             };
-            root = {
+            luks = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "luks";
+                name = "crypted";
+                askPassword = true;
+                settings = {
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = ["compress=zstd" "noatime" "nodiratime"];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = ["compress=zstd" "noatime" "nodiratime"];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = ["compress=zstd" "noatime" "nodiratime"];
+                    };
+                  };
+                };
               };
             };
           };
